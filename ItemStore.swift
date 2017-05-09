5f58630f5647829efe10217c73f1eb91bf0901cf
Zack Falgout <zackfalgout@Zacks-MacBook-Pro.local>
2017-05-08 22:21:00 -0500

    Chapter 11 updates.  Never figured out Gold challenge.  Rows can move below what's supposed to be the final row and then when they're moved back above it, the app crashes.

--- ItemStore.swift
+++ ItemStore.swift
@@ -11,11 +11,6 @@ import UIKit
 class ItemStore{
     var allItems = [Item]()
     
-    init(){
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
     
     @discardableResult func createItem() -> Item{
         let newItem = Item(random: true)
@@ -23,5 +18,28 @@ class ItemStore{
         allItems.append(newItem)
         
         return newItem
+    }
+    
+    //Chapter 11
+    func removeItem(_ item: Item)  {
+        if let index = allItems.index(of: item){
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int){
+        let count = allItems.count
+        if (fromIndex == toIndex || toIndex >= count){
+            return
+        }
+        
+        //Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        
+        //Remove item from array
+        allItems.remove(at: fromIndex)
+        
+        //Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
     }
 }
--- ItemsViewController.swift
+++ ItemsViewController.swift
@@ -11,8 +11,74 @@ import UIKit
 class ItemsViewController: UITableViewController{
     var itemStore: ItemStore!
     
-    @IBAction func addNewItem(_ sender: UIButton){
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count + 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Create an instance of UITableViewCell, with defaultappearance
+        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
         
+        if indexPath.row < itemStore.allItems.count{
+            //Get a recycled cell if one exists.  If not, create one.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+            //Set the text on the cell with the description of the item
+            //that is at the nth index of items, where n = row this cell will appear in on the tableview
+            let item = itemStore.allItems[indexPath.row]
+        
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            return cell
+        } else {
+            let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell_NMI")
+            cell.textLabel?.text = "No more Items!"
+            cell.isUserInteractionEnabled = false
+            cell.textLabel?.isEnabled = false
+            return cell
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if (indexPath.row >= itemStore.allItems.count){
+            return false
+        }
+        return true
+    }
+    
+    //Loading methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Get the height of the status bar
+        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+        
+    }
+    
+    //Chapter 11 code
+    @IBAction func addNewItem(_ sender: UIButton){
+        //Make a new index path for the 0th section, last row.
+        //        let lastRow = tableView.numberOfRows(inSection: 0)
+        //        let indexPath = IndexPath(row: lastRow, section: 0)
+        //
+        //        //Insert this new row into the table
+        //        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        //Create a new item and add it to the stroe
+        let newItem = itemStore.createItem()
+        
+        //Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem){
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            //Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
     }
     
     @IBAction func toggleEditingMode(_ sender: UIButton){
@@ -32,36 +98,74 @@ class ItemsViewController: UITableViewController{
         }
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        //If the table view is asking to commit a delete command...
+        if editingStyle == .delete{
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            //Changed "Delete" to "Remove" for bronze challenge.
+            let deleteAction = UIAlertAction(title: "Remove", style: .destructive, handler: { (action) -> Void in
+                
+                //print("The number of items in the itemStore is:  \(self.itemStore.allItems.count)")
+                //Remove item from the store
+                self.itemStore.removeItem(item)
+                
+                //Also remove that row from the table view with an animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+        
+            ac.addAction(deleteAction)
+                
+            //Present the alert controller
+            present(ac, animated: true, completion: nil)
+            
+            //Remove the item from the store
+            itemStore.removeItem(item)
+            
+            //Also remove that row from the table view with an animation
+            //tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //Create an instance of UITableViewCell, with defaultappearance
-        //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath){
         
-        //Get a recycled cell if one exists.  If not, create one.
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        
-        //Set the text on the cell with the description of the item
-        //that is at the nth index of items, where n = row this cell will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
-        
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        
-        return cell
+        if destinationIndexPath.row >= itemStore.allItems.count{
+            //Update the model
+            itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        } else {
+            return
+        }
     }
     
-    //Loading methods
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //Get the height of the status bar
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
